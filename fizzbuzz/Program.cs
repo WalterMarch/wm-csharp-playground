6824175d0b4925ce3c49abc0f3ec25909e1fb47d
WalterMarch <walter.march@outlook.com>
2025-01-30 08:55:27 +0000

    add fizzbuzz

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+int MAX = 100;
+
+for (int i = 1; i <= MAX; i++) {
+    string output = "";
+    if (i % 3 == 0) {output += "Fizz";}
+    if (i % 5 == 0) {output += "Buzz";}
+    Console.WriteLine(
+        output == "" ? i : output
+    );
+}
