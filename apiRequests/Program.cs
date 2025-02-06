1481f2a876e9c4a8417b1314e36e9fb183f2838b
WalterMarch <walter.march@outlook.com>
2025-02-06 10:14:45 +0000

    add GET request

---
+++ apiRequests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json")
+);
+
+await ProcessRepositoriesAsync(client);
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    var json = await client.GetStringAsync("https://ok.surf/api/v1/news-section-names");
+    Console.Write(json);
+}
